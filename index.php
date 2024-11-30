b48b855ed7b5952ced93b6ccc99883683de0da96
Nerots <daniuyan71@gmail.com>
2024-12-01 01:06:16 +0700

    chore: rename core to cores

--- index.php
+++ index.php
@@ -2,17 +2,15 @@
 
 use app\controllers\Auth;
 use app\controllers\Home;
-use app\core\Database;
-use app\core\Router;
+use app\cores\Database;
+use app\cores\Router;
+use app\constant\Config;
+use app\middlewares\AuthMiddleware;
 
 require_once "helpers/env.php";
 require_once "vendor/autoload.php";
 
-use app\constant\Config;
-use app\middlewares\AuthMiddleware;
-
 $db = new Database(Config::getConfig());
-$db->connect();
 
 $app = new Router();
 
@@ -21,3 +19,5 @@ $app::get("/login", [Auth::class, "renderLogin"]);
 $app::post("/post-login", [Auth::class, "login"]);
 
 $app::run();
+
+
