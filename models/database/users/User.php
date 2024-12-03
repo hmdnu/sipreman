0caeed60c3dc7b5057659241e3c2d94f649a37fc
Nerots <daniuyan71@gmail.com>
2024-12-03 08:50:29 +0700

    feat: insertion deletion user

--- models/database/users/User.php
+++ models/database/users/User.php
@@ -2,6 +2,9 @@
 
 namespace app\models\database\users;
 
+use app\cores\Blueprint;
+use app\cores\Schema;
+
 class User
 {
     public const TABLE = "user";
@@ -9,4 +12,20 @@ class User
     public const NO_INDUK = "no_induk";
     public const PASSWORD = "password";
 
+    public static function insert(array $data): array
+    {
+        return Schema::insertInto(self::TABLE, function (Blueprint $table) use ($data) {
+            $table->insert([self::ID, self::NO_INDUK, self::PASSWORD], $data);
+        });
+    }
+
+    public static function findOne(string $noInduk, string $password)
+    {
+
+    }
+
+    public static function deleteAll(): array
+    {
+        return Schema::deleteFrom(self::TABLE);
+    }
 }
