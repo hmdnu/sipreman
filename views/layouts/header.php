b375ff6fa0510e58b89b4846197c39f202e2a007
Nerots <daniuyan71@gmail.com>
2024-12-03 08:48:09 +0700

    chore: change css path

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php echo \app\cores\View::getTitle() ?></title>
-    <link rel="stylesheet" href="./public/css/output.css">
+    <link rel="stylesheet" href="/public/css/output.css">
 
 </head>
 <body>
