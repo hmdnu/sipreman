193398b9255c760d91c82459dafc26f24dc864f3
Nerots <daniuyan71@gmail.com>
2024-12-03 08:56:55 +0700

    delete: register.php

---
+++ views/register/register.php
@@ -0,0 +1,15 @@
+<h1>Register page</h1>
+
+
+<form action="/register-post" method="post" class="">
+
+    <label for="no-induk">
+        <input type="text" name="no-induk" placeholder="no induk">
+    </label>
+
+    <label for="password">
+        <input type="password" name="password" placeholder="password">
+    </label>
+
+    <button type="submit">Submit</button>
+</form>
