46d42dde8e439c3b3d2248917cb2c0106404ccbe
Nerots <daniuyan71@gmail.com>
2024-12-03 08:40:58 +0700

    feat: admin seeder

---
+++ models/seeders/AdminSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+
+use app\models\BaseSeeder;
+
+class AdminSeeder implements BaseSeeder
+{
+    public function create(): array
+    {
+        $data = [];
+    }
+
+    public function delete(): array
+    {
+        // TODO: Implement delete() method.
+    }
+
+
+}
