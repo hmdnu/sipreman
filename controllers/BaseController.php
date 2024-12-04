aa4702589f7b8a0ba2cc6bfdd643de408936054e
Nerots <daniuyan71@gmail.com>
2024-12-04 07:52:36 +0700

    refactor: delete instance of request response in BaseController

--- controllers/BaseController.php
+++ controllers/BaseController.php
@@ -8,14 +8,10 @@ use app\cores\View;
 
 class BaseController
 {
-    protected Request $request;
-    protected Response $response;
     private View $view;
 
     public function __construct()
     {
-        $this->request = new Request();
-        $this->response = new Response();
         $this->view = new View();
     }
 
