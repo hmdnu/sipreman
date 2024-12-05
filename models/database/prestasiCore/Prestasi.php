5ace4ac4037f7535c1c8e7646ff6b54f6800abf3
Nerots <daniuyan71@gmail.com>
2024-12-05 15:04:55 +0700

    refactor: prestasi model

--- models/database/prestasiCore/Prestasi.php
+++ models/database/prestasiCore/Prestasi.php
@@ -26,9 +26,9 @@ class Prestasi extends BaseModel
 
     public static function insert(array $data): array
     {
-        return Schema::insertInto(self::TABLE, function(Blueprint $table) use ($data){
+        return Schema::insertInto(self::TABLE, function (Blueprint $table) use ($data) {
             $table->insert([self::ID, self::NIM, self::COMPETITION_NAME, self::CATEGORY_NAME, self::COMPETITION_LEVEL, self::PLACE, self::DATE_START_COMPETITION, self::DATE_END_COMPETITION, self::COMPETITION_SOURCE,
-                            self::TOTAL_COLLEGE_ATTENDED, self::TOTAL_PARTICIPANT, self::IS_VALIDATE, self::ATTACHMENT_ID, self::SUPERVISOR_ID], $data);
+                self::TOTAL_COLLEGE_ATTENDED, self::TOTAL_PARTICIPANT, self::IS_VALIDATE, self::ATTACHMENT_ID, self::SUPERVISOR_ID], $data);
         });
     }
 
