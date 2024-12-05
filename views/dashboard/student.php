aba2cc97be9e3edf59007606cabf0b2f070c41dd
Nerots <daniuyan71@gmail.com>
2024-12-05 15:04:04 +0700

    feat: student dashboard

--- views/dashboard/student.php
+++ views/dashboard/student.php
@@ -1,5 +1,7 @@
-<h1>Dashboard mahasiswa</h1>
+<section>
+    <h1>Dashboard mahasiswa</h1>
 
-<form action="/logout" method="post">
-    <button type="submit">Logout</button>
-</form>
+    <form action="/logout" method="post">
+        <button type="submit" class="">Logout</button>
+    </form>
+</section>
