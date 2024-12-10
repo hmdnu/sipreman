a328046c0cc7664849e72e6680c16e48faf3c504
Audricc <audricc015@gmail.com>
2024-12-11 00:21:44 +0700

    feat: add dashboard admin views

--- controllers/Dashboard.php
+++ controllers/Dashboard.php
@@ -8,16 +8,19 @@ use app\models\database\users\Student;
 use app\cores\Session;
 use app\models\database\Major;
 use app\models\database\StudyProgram;
+use app\models\database\users\Admin;
 use app\models\database\users\Lecturer;
 
 class Dashboard extends BaseController
 {
     private string $nim = "";
+    private string $nip = "";
 
     public function __construct()
     {
         parent::__construct();
         $this->nim = Session::get("user");
+        $this->nip = Session::get("user");
     }
 
     public function studentUploadPrestasi(Request $req, Response $res): void
@@ -54,9 +57,34 @@ class Dashboard extends BaseController
         }
     }
 
-    public function renderAdminDashboardValidation(Request $req, Response $res): void
+    public function AdminDashboard(Request $req, Response $res): void
     {
-        $this->view("dashboard/admin/validasi", "Admin validasi");
+        try{
+            $adminData = $this->getAdminData($this->nip);
+            $this->view("dashboard/admin/dashboard", "dashboard", $adminData);
+        } catch (\PDOException $err){
+            var_dump("error bang", $err->getMessage());
+        }
+    }
+
+    public function AdminValidation(Request $req, Response $res): void
+    {
+        try{
+            $adminData = $this->getAdminData($this->nip);
+            $this->view("dashboard/admin/validation", "validasi", $adminData);
+        } catch (\PDOException $err){
+            var_dump("error bang", $err->getMessage());
+        }
+    }
+
+    public function AdminDataStudent(Request $req, Response $res): void
+    {
+        try{
+            $adminData = $this->getAdminData($this->nip);
+            $this->view("dashboard/admin/dataStudent", "data mahasiswa", $adminData);
+        } catch (\PDOException $err){
+            var_dump("error bang", $err->getMessage());
+        }
     }
 
     public function renderUploadTest(Request $req, Response $res): void
@@ -79,6 +107,16 @@ class Dashboard extends BaseController
             "studyPrograms" => $getStudyProgram,
             "supervisors" => $getSupervisors,
             "majors" => $getMajors
+        ];
+    }
+
+    private function getAdminData(string $nip): array
+    {
+        $user = Admin::findOne($nip);
+
+        return[
+            "nip" => $user["nip"],
+            "name" => $user["name"]
         ];
     }
 }
