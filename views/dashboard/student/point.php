19a2b891ffcd198b0eddaca0c1073f16d566e0ec
unknown <vitaekasaraswati16@gmail.com>
2024-12-10 17:45:35 +0700

    feat: add dashboard ui for student poinku

--- views/dashboard/student/point.php
+++ views/dashboard/student/point.php
@@ -95,7 +95,7 @@ use app\cores\View;
                     </thead>
                     <tbody>
                 
-                    <!-- button aksi -->
+                    <!-- button aksi valid & calendar -->
                     <button
                         class="border border-neutral-300 rounded-md px-3 py-2 bg-neutral-100 text-green-500 hover:text-green-700">
                         <img src="/public/assets/icon/validate_icon.png" alt="Validate Icon" class="h-4 w-4">
