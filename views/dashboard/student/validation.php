c9c03002e0c1893ea517a99bcc993e58e85e3883
Nerots <daniuyan71@gmail.com>
2024-12-11 11:00:21 +0700

    feat: hero

--- views/dashboard/student/validation.php
+++ views/dashboard/student/validation.php
@@ -2,6 +2,12 @@
 
 use app\cores\View;
 
+echo "<pre>";
+var_dump(View::getData());
+echo "</pre>";
+exit;
+
+
 ?>
 
 <?php View::renderComponent("dashboard/sidebar"); ?>
--- views/testUpload.php
+++ views/testUpload.php
@@ -1,1 +1,6 @@
-<?php
+<form action="/post-prestasi" method="post" enctype="multipart/form-data" class="flex flex-col">
+    <input type="text" name="name[]" placeholder="name"/>
+    <input type="text" name="name[]" placeholder="name"/>
+    <input type="text" name="name[]" placeholder="name"/>
+    <button type="submit">Submit</button>
+</form>
