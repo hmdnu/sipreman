21100b3896d92789e16ca92f04faac2320661e64
Nerots <daniuyan71@gmail.com>
2024-12-15 09:54:57 +0700

    feat: delete unnecessary files

--- views/testUpload.php
+++
@@ -1,6 +0,0 @@
-<form action="/post-prestasi" method="post" enctype="multipart/form-data" class="flex flex-col">
-    <input type="text" name="name[]" placeholder="name"/>
-    <input type="text" name="name[]" placeholder="name"/>
-    <input type="text" name="name[]" placeholder="name"/>
-    <button type="submit">Submit</button>
-</form>
