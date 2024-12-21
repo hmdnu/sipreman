ff35ef5862a98f908bca31cd0511c50ce7f87d54
Nerots <daniuyan71@gmail.com>
2024-12-21 08:55:35 +0700

    feat: operator constants

---
+++ constant/Operators.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\constant;
+
+
+class Operators
+{
+    public const string EQUAL = "=";
+    public const string NOT_EQUAL = "!=";
+    public const string LESS = "<";
+    public const string GREATER = ">";
+    public const string LESS_EQUAL = "<=";
+    public const string GREATER_EQUAL = ">=";
+}
