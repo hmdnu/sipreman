d0829114415dbfe434b9635bf160c299844945a0
Nerots <daniuyan71@gmail.com>
2024-12-21 08:54:57 +0700

    feat: add dashboard admin components

--- views/components/dashboard/admSidebar.php
+++ views/components/dashboard/admSidebar.php
@@ -17,28 +17,28 @@ use app\cores\View;
         <div class="text-center mb-6">
             <img src="https://via.placeholder.com/100" alt="profile picture"
                 class="w-20 h-20 rounded-full mx-auto mb-2" />
-            <h2 class="text-[20px] font-semibold"><?php echo View::getData()["adminData"]["name"] ?>
+            <h2 class="text-[20px] font-semibold"><?php echo View::getData()["name"] ?>
             </h2>
-            <p class="text-[12px] font-medium text-gray-500"><?php echo View::getData()["adminData"]["nip"] ?></p>
+            <p class="text-[12px] font-medium text-gray-500"><?php echo View::getData()["nip"] ?></p>
         </div>
 
         <!-- side bar -->
         <div class="space-y-4">
             <!-- dashboard admin -->
-            <a href="/dashboard/admin/<?php echo View::getData()["adminData"]["nip"] ?>"
+            <a href="/dashboard/admin/<?php echo View::getData()["nip"] ?>"
                 class="text-[12px] font-m w-full flex items-center gap-2 py-2 px-4 bg-white text-gray-500 border border-gray-300 rounded-lg shadow hover:bg-[#6F38C5] hover:text-white active:bg-[#6F38C5] active:text-white transition duration-300">
                 <img src="/public/assets/icon/adm_dashboard.png" alt="Icon Dashboard Admin" class="h-4 w-4" />
                 Dashboard
             </a>
 
             <!-- student data -->
-            <a href="/dashboard/admin/<?php echo View::getData()["adminData"]["nip"] ?>/validation"
+            <a href="/dashboard/admin/<?php echo View::getData()["nip"] ?>/validation"
                 class="text-[12px] font-medium w-full flex items-center gap-2 py-2 px-4 bg-white text-gray-700 border border-gray-300 rounded-lg shadow hover:bg-[#6F38C5] hover:text-white active:bg-[#6F38C5] active:text-white transition duration-300">
                 <img src="/public/assets/icon/adm_dataStudent.png" alt="Icon Data Student" class="h-4 w-4" />
                 Data Mahasiswa
             </a>
             <!-- validation -->
-            <a href="/dashboard/admin/<?php echo View::getData()["adminData"]["nip"] ?>/point"
+            <a href="/dashboard/admin/<?php echo View::getData()["nip"] ?>/point"
                 class="text-[12px] font-medium w-full flex items-center gap-2 py-2 px-4 bg-white text-gray-700 border border-gray-300 rounded-lg shadow hover:bg-[#6F38C5] hover:text-white active:bg-[#6F38C5] active:text-white transition duration-300">
                 <img src="/public/assets/icon/adm_validasi.png" alt="Icon Validation Admin" class="h-4 w-4" />
                 Validasi
