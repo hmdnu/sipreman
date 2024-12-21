d3642f8ae78dcd1bd3f9fb87cc03e963f2d611c6
Nerots <daniuyan71@gmail.com>
2024-12-21 08:55:11 +0700

    feat: add validation student dashboard

--- views/dashboard/student/validation.php
+++ views/dashboard/student/validation.php
@@ -2,10 +2,6 @@
 
 use app\cores\View;
 
-//
-//var_dump(View::getData()["prestasiData"]);
-//exit;
-
 ?>
 
 <?php View::renderComponent("dashboard/sidebar", View::getData()); ?>
@@ -19,7 +15,7 @@ use app\cores\View;
             <!-- Search Bar -->
             <div class="relative w-64">
                 <input type="text" placeholder="Cari"
-                       class="w-full pl-10 pr-4 py-2 text-[12px] border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500">
+                    class="w-full pl-10 pr-4 py-2 text-[12px] border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500">
                 <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
                     <i class="fas fa-search text-gray-400"></i>
                 </div>
@@ -63,24 +59,24 @@ use app\cores\View;
         <div class="overflow-x-auto">
             <table class="min-w-full bg-white">
                 <thead>
-                <tr class="bg-[#6F38C5] text-white text-[12px]">
-                    <th class="px-4 py-2 text-center">No</th>
-                    <th class="px-4 py-2 text-left">Nama Kegiatan</th>
-                    <th class="px-4 py-2 text-left">No Surat Tugas</th>
-                    <th class="px-4 py-2 text-center">Tingkat</th>
-                    <th class="px-4 py-2 text-center">Peran</th>
-                    <th class="px-4 py-2 text-center">Poin</th>
-                    <th class="px-4 py-2 text-center">Aksi</th>
-                </tr>
+                    <tr class="bg-[#6F38C5] text-white text-[12px]">
+                        <th class="px-4 py-2 text-center">No</th>
+                        <th class="px-4 py-2 text-left">Nama Kegiatan</th>
+                        <th class="px-4 py-2 text-left">No Surat Tugas</th>
+                        <th class="px-4 py-2 text-center">Tingkat</th>
+                        <th class="px-4 py-2 text-center">Peran</th>
+                        <th class="px-4 py-2 text-center">Poin</th>
+                        <th class="px-4 py-2 text-center">Aksi</th>
+                    </tr>
                 </thead>
 
                 <tbody class="text-[12px]">
 
-                <?php
-                $prestasiData = View::getData()["prestasiData"][0];
+                    <?php
+                    $prestasiData = View::getData()["prestasiData"][0];
 
-                for ($i = 0; $i < count($prestasiData); $i++) {
-                    echo "<tr class='border-b'>
+                    for ($i = 0; $i < count($prestasiData); $i++) {
+                        echo "<tr class='border-b'>
                             <td class='px-4 py-2 text-center'>" . ($i + 1) . "</td>
                             <td class='px-4 py-2'> {$prestasiData[$i]["competition_name"]}</td>
                             <td class='px-4 py-2'>{$prestasiData[$i]["loa_number"]}</td>
@@ -88,8 +84,8 @@ use app\cores\View;
                             <td class='px-4 py-2 text-center'>{$prestasiData[$i]["role"]}</td>
                             <td class='px-4 py-2 text-center'>{$prestasiData[$i]["point"]}</td>
                             <td class='px-4 py-2'>";
-                    if ($prestasiData[$i]["is_validate"] === "1") {
-                        echo "<div class='flex items-center justify-center gap-2'>                                 
+                        if ($prestasiData[$i]["is_validate"] === "1") {
+                            echo "<div class='flex items-center justify-center gap-2'>                                 
                                 <button class='p-1.5 bg-green-100 text-green-800 rounded-lg hover:bg-green-200' title='Tervalidasi'>
                                     <img src='/public/assets/icon/validate_icon.png' alt='Valid' class='h-5 w-5'>
                                 </button>
@@ -99,8 +95,8 @@ use app\cores\View;
                             </div>
                             </td>
                         </tr>";
-                    } else {
-                        echo "<div class='flex items-center justify-center gap-2'>                                 
+                        } else {
+                            echo "<div class='flex items-center justify-center gap-2'>                                 
                                 <button class='p-1.5 bg-grey-100 text-red-800 rounded-lg hover:bg-red-200' title='Ditolak'>  
                                     <img src='/public/assets/icon/notvalidate_icon.png' alt='Ditolak' class='h-5 w-5'>  
                                 </button>  
@@ -110,9 +106,9 @@ use app\cores\View;
                             </div>
                             </td>
                         </tr>";
+                        }
                     }
-                }
-                ?>
+                    ?>
 
                 </tbody>
             </table>
