4a47dd745aa02f3f1177752aa377e5cd59a51fa4
Nerots <daniuyan71@gmail.com>
2024-12-22 11:42:45 +0700

    feat: raw query builder

--- cores/dbal/Query.php
+++ cores/dbal/Query.php
@@ -5,14 +5,32 @@ namespace app\cores\dbal;
 class Query extends BaseConstruct
 {
     private string $sql;
+    private array $params = [];
 
     public function __construct(string $sql)
     {
         $this->sql = $sql;
     }
 
-    public function execute(): ?array
+    public function bindParams(string $params, string $value): self
     {
-        return $this->executeSql($this->sql);
+        $this->params[$params] = $value;
+        return $this;
+    }
+
+    public function execute(): bool
+    {
+        return $this->executeSql($this->sql, $this->params);
+    }
+
+    public function fetch(): array
+    {
+        $res = $this->execute();
+
+        if (!$res) {
+            return [];
+        }
+
+        return $this->fetchColumn();
     }
 }
