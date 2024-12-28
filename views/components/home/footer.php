35ce5711da3fc71bb6ff7477dc264c3d3f006b8a
yonandamayla <yonandamayla555@gmail.com>
2024-12-28 13:18:59 +0700

    refactor(footer): update footer layout and content

---
+++ views/components/home/footer.php
@@ -0,0 +1,36 @@
+<footer class="bg-primary-900 w-full mt-20">
+    <div class="w-full max-w-screen-lg mx-auto p-4 md:py-8">
+        <div class="sm:flex sm:items-center sm:justify-between">
+            <div class="flex flex-col items-start mb-10 sm:mb-0 space-y-3 rtl:space-y-reverse">
+                <a href="/login" class="flex items-center space-x-3 rtl:space-x-reverse">
+                    <img src="/public/assets/icon/graduation_white.png" class="h-8" />
+                    <span class="self-center h3 mt-2 font-medium whitespace-nowrap text-neutral-0">Sipreman</span>
+                </a>
+                <p class="text-neutral-300 font-regular text-left p2 w-[350px] py-1">Jl. Soekarno Hatta No.9, Jatimulyo, Kec. Lowokwaru, Kota Malang, Jawa Timur 65141</p>
+                <div class="flex items-center space-x-1">
+                    <img src="/public/assets/icon/telephone.png" class="w-4 h-4 mr-2" alt="Telephone Icon" />
+                    <p class="text-neutral-300 font-regular text-left p2">+62 821 4294 0921 (Agus)</p>
+                </div>
+            </div>
+            <div class="flex flex-col items-start">
+                <p class="text-neutral-300 font-medium mb-2 p1">Akses Cepat</p>
+                <ul class="flex flex-wrap items-center mb-6 h5 font-medium text-neutral-0 sm:mb-0">
+                    <li>
+                        <a href="#hero" class="hover:underline me-4 md:me-6">Home</a>
+                    </li>
+                    <li>
+                        <a href="#about" class="hover:underline me-4 md:me-6">About</a>
+                    </li>
+                    <li>
+                        <a href="#service" class="hover:underline me-4 md:me-6">Service</a>
+                    </li>
+                    <li>
+                        <a href="#achievement" class="hover:underline">Juara</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <hr class="my-6 sm:mx-auto bg-neutral-0 lg:my-8" />
+        <span class="block p1 text-neutral-300 text-center">© 2024 <a href="/login" class="hover:underline">Sipreman™</a>. All Rights Reserved.</span>
+    </div>
+</footer>
