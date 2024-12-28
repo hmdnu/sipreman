31e0bb6222650620fddf0ed899d8605fa2ad5360
Nerots <daniuyan71@gmail.com>
2024-12-28 12:39:35 +0700

    feat: drop view

--- cores/dbal/dml/View.php
+++ cores/dbal/dml/View.php
@@ -6,30 +6,63 @@ use app\cores\dbal\BaseConstruct;
 
 class View extends BaseConstruct
 {
-    private string $sql;
+    private string $sql = "";
+    private string $viewName;
 
     public function __construct(string $viewName)
     {
-        $this->sql = "IF NOT EXISTS (
+        $this->viewName = $viewName;
+    }
+
+    private function buildView(): string
+    {
+        return "IF NOT EXISTS (
             SELECT 1
             FROM sys.views
-            WHERE name = '$viewName'
+            WHERE name = '$this->viewName'
         ) 
         BEGIN
             EXEC sp_executesql N'
-            CREATE VIEW $viewName AS ";
+            CREATE VIEW $this->viewName AS ";
     }
 
     public function as(string $sql): self
     {
-        $this->sql .= "$sql' END;";
+        $sql = str_replace(";", "", $sql);
+
+        $sql = preg_replace_callback(
+            '/WHERE\s+([^\s]+)\s*=\s*\'([^\']+)\'/i',
+            function ($matches) {
+                $column = $matches[1];
+                $value = str_replace("'", "''", $matches[2]);
+                return "WHERE $column = ''$value''";
+            },
+            $sql
+        );
+
+        $this->sql .= "$sql;' END;";
 
         return $this;
     }
 
 
+    public function drop(): self
+    {
+
+        $this->sql = "DROP VIEW $this->viewName;";
+
+        return $this;
+    }
+
     public function execute(): bool
     {
-        return $this->executeSql($this->sql);
+        if (preg_match('/^\s*(DROP)/i', $this->sql)) {
+            return $this->executeSql($this->sql);
+        }
+
+        $sql = $this->buildView();
+        $sql .= $this->sql;
+
+        return $this->executeSql($sql);
     }
 }
