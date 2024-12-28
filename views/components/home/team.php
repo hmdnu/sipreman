c58bb5bca935365c05d21dfaf6dee241a1552d21
yonandamayla <yonandamayla555@gmail.com>
2024-12-28 10:53:19 +0700

    fix(team): improve layout and alignment of team developer page

--- views/components/home/team.php
+++ views/components/home/team.php
@@ -1,40 +1,37 @@
-<section class="w-[1440px] h-[500px] bg-neutral-0 justify-center gap-[20px] z-10">
-
+<section class="w-full h-[500px] bg-neutral-0 flex flex-col items-center justify-center gap-[20px] z-10">
     <!-- the creators -->
-    <p class="h4 text-center pt-3">INTRODUCTION TO </p>
-    <p class="h1 text-center mt-[20px]">The Creators and Developers</p>
+    <p class="h5 text-center pt-3 text-primary-600">INTRODUCTION TO </p>
+    <p class="h1 text-center mt-[3px] font-semibold">The Creators and Developers</p>
 
     <div class="flex justify-center m-[50px] space-x-12">
-
         <div class="flex flex-col items-center">
-            <img src="\public\assets\img\audric_img.png" alt="Creator 2" class="w-[170px] h-[170px]">
+            <img src="/public/assets/img/audric_img.png" alt="Creator 2" class="w-[170px] h-[170px]">
             <p class="mt-3 h5 font-semibold">M. Audric A.</p>
-            <p class="mt-2 p1 text-primary-600">back end</p>
+            <p class="mt-2 p1 text-primary-600">Back-End Developer</p>
         </div>
 
         <div class="flex flex-col items-center">
-            <img src="\public\assets\img\hamdan_img.png" alt="Creator 1" class="w-[170px] h-[170px]">
+            <img src="/public/assets/img/hamdan_img.png" alt="Creator 1" class="w-[170px] h-[170px]">
             <p class="mt-3 h5 font-semibold">M. Hamdan U.</p>
-            <p class="mt-2 p1 text-primary-600">back end</p>
+            <p class="mt-2 p1 text-primary-600">Back-End Developer</p>
         </div>
 
         <div class="flex flex-col items-center">
-            <img src="\public\assets\img\fauzi_img.png" alt="Creator 3" class="w-[170px] h-[170px]">
+            <img src="/public/assets/img/fauzi_img.png" alt="Creator 3" class="w-[170px] h-[170px]">
             <p class="mt-3 h5 font-semibold">M. Reishi F.</p>
-            <p class="mt-2 p1 text-primary-600">front end</p>
+            <p class="mt-2 p1 text-primary-600">Front-End Developer</p>
         </div>
 
         <div class="flex flex-col items-center">
-            <img src="\public\assets\img\vita_img.png" alt="Creator 4" class="w-[170px] h-[170px]">
+            <img src="/public/assets/img/vita_img.png" alt="Creator 4" class="w-[170px] h-[170px]">
             <p class="mt-3 h5 font-semibold">Vita Eka S.</p>
-            <p class="mt-2 p1 text-primary-600">front end</p>
+            <p class="mt-2 p1 text-primary-600">Front-End Developer</p>
         </div>
 
         <div class="flex flex-col items-center">
-            <img src="\public\assets\img\yonanda_img.png" alt="Creator 5" class="w-[170px] h-[170px]">
+            <img src="/public/assets/img/yonanda_img.png" alt="Creator 5" class="w-[170px] h-[170px]">
             <p class="mt-3 h5 font-semibold">Yonanda M.</p>
-            <p class="mt-2 p1 text-primary-600">project manager</p>
+            <p class="mt-2 p1 text-primary-600">Project Manager</p>
         </div>
     </div>
-
 </section>
