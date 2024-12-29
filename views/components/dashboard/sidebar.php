6496a7e67ae54dd433334d272aef3198756e4fb2
Nerots <daniuyan71@gmail.com>
2024-12-30 00:46:46 +0700

    skibidi sigma

--- views/components/dashboard/sidebar.php
+++ views/components/dashboard/sidebar.php
@@ -1,8 +1,10 @@
 <?php
 
+use app\cores\Request;
 use app\cores\View;
 
-$currentUrl = $_SERVER['REQUEST_URI'];
+$instance = new Request();
+$currentUrl = $instance->getUrl();
 
 ?>
 
@@ -17,8 +19,7 @@ $currentUrl = $_SERVER['REQUEST_URI'];
 
         <!-- user profile -->
         <div class="text-center mb-6">
-            <img src="/public/assets/img/user.png" alt="profile picture"
-                class="w-20 h-20 rounded-full mx-auto mb-2" />
+            <img src="/public/assets/img/user.png" alt="profile picture" class="w-20 h-20 rounded-full mx-auto mb-2" />
             <h2 class="h5 font-semibold mt-3"><?php echo View::getData()["studentData"]["name"] ?>
             </h2>
             <p class="p1 font-medium text-gray-500 mt-2"><?php echo View::getData()["studentData"]["nim"] ?></p>
@@ -29,21 +30,24 @@ $currentUrl = $_SERVER['REQUEST_URI'];
             <!-- upload -->
             <a href="/dashboard/student/<?php echo View::getData()["studentData"]["nim"] ?>"
                 class="sidebar-link text-[12px] font-medium w-full flex items-center gap-4 py-3 px-4 <?php echo strpos($currentUrl, '/dashboard/student/' . View::getData()["studentData"]["nim"]) !== false && strpos($currentUrl, '/validation') === false && strpos($currentUrl, '/point') === false ? 'bg-primary-500 text-white' : 'bg-white text-gray-700'; ?>  rounded-lg shadow transition duration-300">
-                <img src="<?php echo strpos($currentUrl, '/dashboard/student/' . View::getData()["studentData"]["nim"]) !== false && strpos($currentUrl, '/validation') === false && strpos($currentUrl, '/point') === false ? '/public/assets/icon/unggah_white.png' : '/public/assets/icon/upload_icon.png'; ?>" alt="Icon Upload" class="h-4 w-4" />
+                <img src="<?php echo strpos($currentUrl, '/dashboard/student/' . View::getData()["studentData"]["nim"]) !== false && strpos($currentUrl, '/validation') === false && strpos($currentUrl, '/point') === false ? '/public/assets/icon/unggah_white.png' : '/public/assets/icon/upload_icon.png'; ?>"
+                    alt="Icon Upload" class="h-4 w-4" />
                 Unggah Prestasi
             </a>
 
             <!-- validation -->
             <a href="/dashboard/student/<?php echo View::getData()["studentData"]["nim"] ?>/validation"
                 class="sidebar-link text-[12px] font-medium w-full flex items-center gap-4 py-3 px-4 <?php echo strpos($currentUrl, '/validation') !== false ? 'bg-primary-500 text-white' : 'bg-white text-gray-700'; ?>  rounded-lg shadow transition duration-300">
-                <img src="<?php echo strpos($currentUrl, '/validation') !== false ? '/public/assets/icon/validation_white.png' : '/public/assets/icon/validation_icon.png'; ?>" alt="icon validation" class="h-4 w-4" />
+                <img src="<?php echo strpos($currentUrl, '/validation') !== false ? '/public/assets/icon/validation_white.png' : '/public/assets/icon/validation_icon.png'; ?>"
+                    alt="icon validation" class="h-4 w-4" />
                 Cek Validasi
             </a>
 
             <!-- my points -->
             <a href="/dashboard/student/<?php echo View::getData()["studentData"]["nim"] ?>/point"
                 class="sidebar-link text-[12px] font-medium w-full flex items-center gap-4 py-3 px-4 <?php echo strpos($currentUrl, '/point') !== false ? 'bg-primary-500 text-white' : 'bg-white text-gray-700'; ?> rounded-lg shadow transition duration-300">
-                <img src="<?php echo strpos($currentUrl, '/point') !== false ? '/public/assets/icon/poinku_white.png' : '/public/assets/icon/point_icon.png'; ?>" alt="icon poin" class="h-4 w-4" />
+                <img src="<?php echo strpos($currentUrl, '/point') !== false ? '/public/assets/icon/poinku_white.png' : '/public/assets/icon/point_icon.png'; ?>"
+                    alt="icon poin" class="h-4 w-4" />
                 Poinku
             </a>
         </div>
