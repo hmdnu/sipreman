9e91bb09927dd50b2628c0dbef1d3ba56d91c2c6
Nerots <daniuyan71@gmail.com>
2024-12-29 21:36:37 +0700

    chore: whitespace changes

--- views/dashboard/student/upload.php
+++ views/dashboard/student/upload.php
@@ -18,14 +18,14 @@ use app\cores\View;
             <div class="flex justify-start items-center mt-8 space-x-4">
                 <!-- button save-->
                 <button
-                        class="flex items-center text-[14px] font-semibold bg-[#49B195] text-white py-2 px-5 rounded-md hover:bg-[#6F38C5] transition shadow-lg">
+                    class="flex items-center text-[14px] font-semibold bg-[#49B195] text-white py-2 px-5 rounded-md hover:bg-[#6F38C5] transition shadow-lg">
                     <img src="/public/assets/icon/save_icon.png" alt="save icon" class="w-4 h-5 mr-2">
                     Simpan
                 </button>
 
                 <!-- button back -->
                 <button type="submit"
-                        class="flex items-center text-[14px] font-semibold bg-gray-200 text-gray-700 py-2 px-5 rounded-md hover:bg-gray-300 transition shadow-lg">
+                    class="flex items-center text-[14px] font-semibold bg-gray-200 text-gray-700 py-2 px-5 rounded-md hover:bg-gray-300 transition shadow-lg">
                     <img src="/public/assets/icon/back_icon.png" alt="back Icon" class="w-5 h-5 mr-2">
                     Kembali
                 </button>
@@ -35,9 +35,9 @@ use app\cores\View;
 </section>
 
 <script type="module">
-    import fragments from "/public/js/fragments/studentDashboard.js"
+import fragments from "/public/js/fragments/studentDashboard.js"
 
-    <?php
+<?php
     $studyPrograms = array_map(function ($studyProgram) {
         return $studyProgram["study_program_name"];
     }, View::getData()["studentData"]["studyPrograms"]);
@@ -51,35 +51,30 @@ use app\cores\View;
     }, View::getData()["studentData"]["majors"]);
     ?>
 
-    const supervisors = <?php echo json_encode($supervisors) ?>;
-    const studyPrograms = <?php echo json_encode($studyPrograms) ?>;
-    const majors = <?php echo json_encode($majors) ?>;
+const supervisors = <?php echo json_encode($supervisors) ?>;
+const studyPrograms = <?php echo json_encode($studyPrograms) ?>;
+const majors = <?php echo json_encode($majors) ?>;
 
-    $(() => {
-        const states = {
-            student: 1,
-            supervisor: 1
-        }
+$(() => {
+    const states = {
+        student: 1,
+        supervisor: 1
+    }
 
-        $("#add-student-btn").on("click", () => {
-            states.student++;
-            $("#container-student-input").append(fragments.getStudentInputFragment(states.student,
-                studyPrograms, majors));
-        })
-
-        $("#add-supervisor-btn").on("click", () => {
-            states.supervisor++;
-            $("#container-supervisor-input").append(fragments.getSupervisorInputFragment(states
-                .supervisor, supervisors));
-        })
-
-        $("#logout-form").on("submit", (e) => {
-            const confirmation = confirm("Logout?");
-
-            if (!confirmation) {
-                e.preventDefault();
-            }
-        })
-
+    $("#add-student-btn").on("click", () => {
+        states.student++;
+        $("#container-student-input").append(fragments.getStudentInputFragment(states.student,
+            studyPrograms, majors));
     })
+
+
+    $("#logout-form").on("submit", (e) => {
+        const confirmation = confirm("Logout?");
+
+        if (!confirmation) {
+            e.preventDefault();
+        }
+    })
+
+})
 </script>
